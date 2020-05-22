8f14946142d16a7cf43be4fc60d862b21f0db142
CadbyLab <48517894+CadbyLab@users.noreply.github.com>
2020-05-22 14:07:47 +0100

    Added the tornado fx section.  defined the values

--- src/main/kotlin/loader.kt
+++ src/main/kotlin/loader.kt
@@ -1,3 +1,5 @@
+import javafx.beans.property.SimpleIntegerProperty
+import javafx.beans.property.SimpleStringProperty
 import java.io.File
 import java.io.FileInputStream
 import java.io.RandomAccessFile
@@ -5,6 +7,13 @@ import java.nio.ByteBuffer
 import java.nio.ByteOrder
 import java.nio.ShortBuffer
 import tornadofx.*
+import javafx.scene.paint.Color
+import tornadofx.Stylesheet.Companion.button
+
+class values(name: String, value: Int){
+    val nameProperty = SimpleStringProperty(this, "Name", name)
+    val valueProperty = SimpleIntegerProperty(this, "Value", value)
+}
 
 @ExperimentalStdlibApi
 fun get_file_params(filename: String):Triple<Int, Int, Int> {
@@ -38,8 +47,28 @@ fun get_data(filename: String, params: Triple<Int, Int, Int>, legnth: Int): Shor
 class MyApp: App(MyView::class)
 
 class MyView: View(){
-        override val root = vbox{
-            button("Press me")
+        override val root = borderpane {
+            top = hbox{
+                label("file name"){
+                    textFill = Color.AZURE
+                }
+            }
+            left = vbox {
+                button ("load")
+                button ("get para")
+                button ("get block data")
+                button (" get block")
+            }
+            center = tableview<values> {
+                items = listOf(
+                    values(" Width", 0),
+                    values("Height", 0),
+                    values("number frames",0),
+                values("Block Length", 10)).observable()
+
+                column("paramater", values::nameProperty)
+                column("Values", values::valueProperty)
+            }
         }
     }
 
